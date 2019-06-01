323a2e8efa016dbffdb1e4b0c3f98b98828dbc40
Ade Niyi <adesanoye.samson@yahoo.com>
2019-06-01 04:47:58 -0700

    created the relationship between levelD and student model

--- app/LevelD.php
+++ app/LevelD.php
@@ -2,9 +2,13 @@
 
 namespace App;
 
+use App\Student;
 use Illuminate\Database\Eloquent\Model;
 
 class LevelD extends Model
 {
-    //
+    public function students()
+    {
+    	return $this->hasMany(Student::class);
+    }
 }
--- app/Student.php
+++ app/Student.php
@@ -2,9 +2,13 @@
 
 namespace App;
 
+use App\LevelD;
 use Illuminate\Database\Eloquent\Model;
 
 class Student extends Model
 {
-    //
+    public function leveld()
+    {
+    	return $this->belongsTo(LevelD::class);
+    }
 }
