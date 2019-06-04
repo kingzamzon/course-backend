5b83374f9be9a18e0cde2a1c9ab5978afacc1fa7
Ade Niyi <adesanoye.samson@yahoo.com>
2019-06-04 07:29:11 -0700

    added primarykey and fillable to lecturer model

--- app/Lecturer.php
+++ app/Lecturer.php
@@ -7,6 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Lecturer extends Model
 {
+	protected $primaryKey = 'lecturer_id';
+
+	protected $fillable = [
+		'username','password'
+	];
+
     public function courses()
     {
     	return $this->hasMany(Course::class);
