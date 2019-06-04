140135d23bd0a93ad89e819d9feb5e45269269a9
Ade Niyi <adesanoye.samson@yahoo.com>
2019-06-04 09:38:50 -0700

    used mutators and accessors to convert username to lowercase

--- app/Lecturer.php
+++ app/Lecturer.php
@@ -9,6 +9,16 @@ class Lecturer extends Model
 {
 	protected $primaryKey = 'lecturer_id';
 
+    public function setUsernameAttribute($username)
+    {
+        $this->attributes['username'] = strtolower($username);
+    }
+
+    public function getUsernameAttribute($username)
+    {
+        return strtolower($username);
+    }
+
 	protected $fillable = [
 		'username','password'
 	];
@@ -17,4 +27,8 @@ class Lecturer extends Model
     {
     	return $this->hasMany(Course::class);
     }
+
+    protected $hidden = [
+    	'password'
+    ];
 }
