3357b1fa81368deb3b4e0fa5a1b4b061b4fe60e6
Ade Niyi <adesanoye.samson@yahoo.com>
2019-06-04 09:32:21 -0700

    planning to convert welcome page to documentation

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -64,6 +64,7 @@
         </style>
     </head>
     <body>
+        how to use the api
         <div class="flex-center position-ref full-height">
             @if (Route::has('login'))
                 <div class="top-right links">
