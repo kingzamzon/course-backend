0598a955ed027e9b51411b68549948d6a06a1ae6
Ade Niyi <adesanoye.samson@yahoo.com>
2019-06-04 09:39:30 -0700

    hide password in student model

--- app/LevelD.php
+++ app/LevelD.php
@@ -9,7 +9,13 @@ use Illuminate\Database\Eloquent\Model;
 class LevelD extends Model
 {
 	protected $table = 'levels_department';
+
+	//set a primarykey from 'id' to leveld_id
 	protected $primaryKey = 'leveld_id';
+
+	protected $fillable = [
+		'level', 'department', 'faculty'
+	];
 
 
 
