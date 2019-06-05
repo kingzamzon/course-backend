c9f73f2153df5575f4001411e239b91652439b18
Ade Niyi <adesanoye.samson@yahoo.com>
2019-06-05 05:41:49 -0700

    handle all error exceptions that may occur

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,11 +3,20 @@
 namespace App\Exceptions;
 
 use Exception;
+use App\Traits\ApiResponser;
+use Illuminate\Database\QueryException;
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Validation\ValidationException;
+use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
 
 class Handler extends ExceptionHandler
 {
+    use ApiResponser;
     /**
      * A list of the exception types that are not reported.
      *
@@ -47,10 +56,72 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
-        if($exception instanceof ValidationException){
+        if($exception instanceof ValidationException)
+        {
             return $this->convertValidationExceptionToResponse($exception, $request);
         }
-        return parent::render($request, $exception);
+
+        if($exception instanceof ModelNotFoundException)
+        {
+            $modelName = strtolower(class_basename($exception->getModel()));
+
+            return $this->errorResponse("Does not exist any {$modelName} with the identificator", 404);
+        }
+
+        if($exception instanceof AuthenticationException)
+        {
+            return $this->unauthenticated($request, $exception);
+        }
+
+        if($exception instanceof AuthorizationException)
+        {
+            return $this->errorResponse($exception->getMessage(), 403);
+        }
+
+        if($exception instanceof MethodNotAllowedHttpException)
+        {
+            return $this->errorResponse('The specified method for the request is invalid', 405);
+        }
+
+        if($exception instanceof NotFoundHttpException)
+        {
+            return $this->errorResponse('The specified URl cannot be found', 404);
+        }
+
+         if($exception instanceof HttpException)
+        {
+            return $this->errorResponse($exception->getMessage(), $exception->getStatusCode());
+        }
+
+         if($exception instanceof QueryException)
+        {
+            $errorCode = $exception->errorInfo[1];
+
+            if($errorCode == 1451){
+
+                return $this->errorResponse('cannot remove this resource permanently. It is related with another resource', 409);
+            }
+        }
+
+        if(config('app.debug'))
+        {
+            return parent::render($request, $exception);
+        }
+
+        return $this->errorResponse('Something went wrong. Try Again', 500);
+
+    }
+
+    /**
+     * Convert an authentication exception into a response.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Auth\AuthenticationException  $exception
+     * @return \Illuminate\Http\Response
+     */
+    protected function unauthenticated($request, AuthenticationException $exception)
+    {
+        return $this->errorResponse("unauthenticated", 401);
     }
 
      /**
@@ -64,7 +135,7 @@ class Handler extends ExceptionHandler
     {
         $errors = $e->validator->errors()->getMessages();
 
-       return response()->json(['error' => $errors]);
+       return $this->errorResponse($errors, 422);
     }
 
 }
