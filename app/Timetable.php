dad1f1d9096a1f8406640476e59cf911113b9aca
Ade Niyi <adesanoye.samson@yahoo.com>
2019-07-02 03:45:57 -0700

    imported timetabletransformer and added primaryid

--- app/Timetable.php
+++ app/Timetable.php
@@ -4,19 +4,24 @@ namespace App;
 
 use App\Course;
 use Illuminate\Database\Eloquent\Model;
+use App\Transformers\TimetableTransformer;
 
 class Timetable extends Model
 {
 	protected $table = 'timetable';
 
+	protected $primaryKey = 'table_id';
+
+	public $transformer = TimetableTransformer::class;
 	protected $fillable = [
 		'course_id', 'type', 'start_time', 'end_time','day', 'venue'
 	];
 
     public function courses()
     {
-    	return $this->belongsTo(Course::class);
+    	return $this->belongsTo(Course::class, 'course_id', 'course_id');
     }
 
+ 
 
 }
