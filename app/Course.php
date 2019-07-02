e8ca342745dbea25fdf5947ea6c6cc9a1520a843
Ade Niyi <adesanoye.samson@yahoo.com>
2019-07-02 02:42:47 -0700

    primary key is set to course_id

--- app/Course.php
+++ app/Course.php
@@ -9,13 +9,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Course extends Model
 {
+    protected $primaryKey = 'course_id';
+
     protected $fillable = [
         'course_code','lecturer_id','leveld_id'
     ];
 
     public function timetables()
     {
-    	return $this->hasMany(Timetable::class);
+    	return $this->hasMany(Timetable::class, 'course_id', 'course_id');
     }
 
     public function leveld()
@@ -25,7 +27,7 @@ class Course extends Model
 
     public function lecturers()
     {
-    	return $this->belongsTo(Lecturer::class);
+    	return $this->belongsTo(Lecturer::class, 'lecturer_id', 'lecturer_id');
     }
 
 }
